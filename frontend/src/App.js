54c0486c806e296e39ac417e120e009571ea5fda
Apples <junyiappleli@gmail.com>
2023-12-22 17:08:18 -0500

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Hello</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
